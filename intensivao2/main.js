95bedea08c8eb9cc4d8ad961d762609ef9e7d380
igorcariello <124227233+igorcariello@users.noreply.github.com>
2023-07-22 12:55:10 -0300

    Add files via upload

---
+++ intensivao2/main.js
@@ -0,0 +1,92 @@
+/*Exercício 1
+
+const hello= alert('Hello World!')
+
+Exercício 2
+
+const number1= Number(prompt('Digite um número:'))
+const number2= Number(prompt('Digite outro número:'))
+
+alert(`A soma dos dois números é: ${number1 + number2}`)
+
+Exercício 3
+
+let putSomething = prompt('Digite alguma coisa:')
+
+
+if (isNaN(Number(putSomething))){
+  alert('Não é um número')
+}else{
+  alert('É um número')
+}
+
+Exercício 4 
+
+let putSomething =  
+
+console.log(typeof putSomething)
+
+if (typeof putSomething == 'string'){
+  alert('É string')
+}else{
+  alert('Não é um string')
+}
+
+Exercício 5
+
+
+let putSomething = 
+
+console.log(typeof putSomething)
+
+if (typeof putSomething == 'boolean'){
+  alert('É um boleano')
+}else{
+  alert('Não é um boleano')
+}
+
+Exercício 6
+
+const number1= Number(prompt('Digite um número:'))
+const number2= Number(prompt('Digite outro número:'))
+
+alert(`A subtração dos dois números é: ${number1 - number2}`)
+
+Exercício 7
+
+const number1= Number(prompt('Digite um número:'))
+const number2= Number(prompt('Digite outro número:'))
+
+alert(`A multiplicação dos dois números é: ${number1 * number2}`)
+
+Exercício 8 
+
+const number1= Number(prompt('Digite um número:'))
+const number2= Number(prompt('Digite outro número:'))
+
+alert(`A divisão dos dois números é: ${number1 / number2}`)
+
+Exercício 
+
+const number1= Number(prompt('Digite um número:'))
+
+if (number1 % 2 ==0){
+  alert(`${number1} é PAR`)
+}else{
+  alert(`${number1} NÃO é PAR`)
+}
+
+Exercício 10
+
+const number1= Number(prompt('Digite um número:'))
+
+if (number1 % 2 != 0){
+  alert(`${number1} é ÍMPAR`)
+}else{
+  alert(`${number1} NÃO é ÍMPAR`)
+}
+
+
+*/
+
+
